df7a865c7fe23fac80fda42de5d5583f8a632350
Josias Santos <josiassantos1577@gmail.com>
2022-04-15 23:53:01 -0300

    formulario 1.0

--- login-page-react-typescript/pages/loginPage/index.tsx
+++ login-page-react-typescript/pages/loginPage/index.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // import "./styles.css";
 
 const LoginPage = () => {
-  const handleSubmit = () => {
-    console.log('submit');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  // Função que permite enviar o formulário quando o botão for pressionado
+  const handleSubmit = (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+    console.log('submit', { email, password });
   };
 
     return (
@@ -13,13 +17,25 @@ const LoginPage = () => {
         <form className="form" onSubmit={handleSubmit}>
           <div className="field">
             <label htmlFor="email">Email</label>
-            <input type="email" name="email" placeholder="email" id="email"/
-            >
+            <input 
+              type="email" 
+              name="email" 
+              placeholder="email" 
+              id="email"
+              value={email} 
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </div>
           <div className="field">
             <label htmlFor="password">Senha</label>
-            <input type="password" name="password" placeholder="password" id="password"/
-            >
+            <input 
+              type="password" 
+              name="password" 
+              placeholder="password" 
+              id="password"
+              value={password} 
+              onChange={(e) => setPassword(e.target.value)}
+            />
           </div>
           <div id="actions">
             <button type="submit">Entrar</button>
